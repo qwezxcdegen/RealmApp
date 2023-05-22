9e4ef82cf07f8691a9ac01cdd0f98938b310e8b8
qwezxcdegen <ifomstep@icloud.com>
2023-05-23 02:37:51 +0500

    Initial Commit

---
+++ RealmApp/ViewControllers/TaskListViewController.swift
@@ -0,0 +1,158 @@
+//
+//  TaskListsViewController.swift
+//  RealmApp
+//
+//  Created by Alexey Efimov on 02.07.2018.
+//  Copyright © 2018 Alexey Efimov. All rights reserved.
+//
+
+import UIKit
+import RealmSwift
+
+final class TaskListViewController: UITableViewController {
+    
+    @IBOutlet weak var sortingControl: UISegmentedControl!
+
+    private var taskLists: Results<TaskList>!
+    private let storageManager = StorageManager.shared
+    private let dataManager = DataManager.shared
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        let addButton = UIBarButtonItem(
+            barButtonSystemItem: .add,
+            target: self,
+            action: #selector(addButtonPressed)
+        )
+        
+        navigationItem.rightBarButtonItem = addButton
+        navigationItem.leftBarButtonItem = editButtonItem
+        
+        taskLists = storageManager.realm.objects(TaskList.self)
+        createTempData()
+        
+        sortingList()
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        tableView.reloadData()
+    }
+    
+    // MARK: - UITableViewDataSource
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        taskLists.count
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "TaskListCell", for: indexPath)
+        var content = cell.defaultContentConfiguration()
+        let taskList = taskLists[indexPath.row]
+        
+        if taskList.tasks.where({ $0.isComplete == false }).count == 0 && taskList.tasks.count > 0 {
+            content.secondaryText = ""
+            cell.accessoryType = .checkmark
+        } else {
+            content.secondaryText = taskList.tasks.where { task in
+                task.isComplete == false
+            }.count.formatted()
+            cell.accessoryType = .none
+        }
+        content.text = taskList.title
+        cell.contentConfiguration = content
+        return cell
+    }
+    
+    // MARK: - UITableViewDelegate
+    override func tableView(_ tableView: UITableView, trailingSwipeActionsConfigurationForRowAt indexPath: IndexPath) -> UISwipeActionsConfiguration? {
+        let taskList = taskLists[indexPath.row]
+        
+        let deleteAction = UIContextualAction(style: .destructive, title: "Delete") { [unowned self] _, _, _ in
+            storageManager.delete(taskList)
+            tableView.deleteRows(at: [indexPath], with: .automatic)
+        }
+        
+        let editAction = UIContextualAction(style: .normal, title: "Edit") { [unowned self] _, _, isDone in
+            showAlert(with: taskList) {
+                tableView.reloadRows(at: [indexPath], with: .automatic)
+            }
+            isDone(true)
+        }
+        
+        let doneAction = UIContextualAction(style: .normal, title: "Done") { [unowned self] _, _, isDone in
+            storageManager.done(taskList)
+            tableView.reloadRows(at: [indexPath], with: .automatic)
+            isDone(true)
+        }
+        
+        editAction.backgroundColor = .orange
+        doneAction.backgroundColor = #colorLiteral(red: 0.3411764801, green: 0.6235294342, blue: 0.1686274558, alpha: 1)
+        
+        return UISwipeActionsConfiguration(actions: [doneAction, editAction, deleteAction])
+    }
+    
+    // MARK: - Navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        guard let indexPath = tableView.indexPathForSelectedRow else { return }
+        guard let tasksVC = segue.destination as? TasksViewController else { return }
+        let taskList = taskLists[indexPath.row]
+        tasksVC.taskList = taskList
+    }
+
+    @IBAction func sortingList() {
+        switch sortingControl.selectedSegmentIndex {
+        case 0:
+            taskLists = taskLists.sorted(byKeyPath: "date", ascending: false)
+            tableView.reloadData()
+        default:
+            taskLists = taskLists.sorted(byKeyPath: "title", ascending: true)
+            tableView.reloadData()
+        }
+    }
+    
+    @objc private func addButtonPressed() {
+        showAlert()
+    }
+    
+    private func createTempData() {
+        if !UserDefaults.standard.bool(forKey: "done") {
+            dataManager.createTempData { [unowned self] in
+                UserDefaults.standard.set(true, forKey: "done")
+                tableView.reloadData()
+            }
+        }
+    }
+}
+
+// MARK: - AlertController
+extension TaskListViewController {
+    private func showAlert(with taskList: TaskList? = nil, completion: (() -> Void)? = nil) {
+        let alertBuilder = AlertControllerBuilder(
+            title: taskList != nil ? "Edit List" : "New List",
+            message: "Please set title for new task list"
+        )
+        
+        alertBuilder
+            .setTextField(taskList?.title)
+            .addAction(title: taskList != nil ? "Update List" : "Save List", style: .default) { [weak self] newValue, _ in
+                if let taskList, let completion {
+                    self?.storageManager.edit(taskList, newValue: newValue)
+                    completion()
+                    return
+                }
+                
+                self?.save(taskList: newValue)
+            }
+            .addAction(title: "Cancel", style: .destructive)
+        
+        let alertController = alertBuilder.build()
+        present(alertController, animated: true)
+    }
+    
+    private func save(taskList: String) {
+        storageManager.save(taskList) { taskList in
+            let rowIndex = IndexPath(row: taskLists.index(of: taskList) ?? 0, section: 0)
+            tableView.insertRows(at: [rowIndex], with: .automatic)
+        }
+    }
+}
